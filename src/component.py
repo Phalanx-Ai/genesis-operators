58dbd49912a62b8cc9c6405602326d95d2a44681
Marek Grac <marek@bedeep.eu>
2023-08-03 15:38:48 +0200

    [feat] Working version

--- src/component.py
+++ src/component.py
@@ -1,21 +1,24 @@
-'''
-Template Component main class.
-
-'''
 import csv
 import logging
-from datetime import datetime
+import datetime
+import sys
+import psutil
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
 
-# configuration variables
-KEY_API_TOKEN = '#api_token'
-KEY_PRINT_HELLO = 'print_hello'
+import time
 
-# list of mandatory parameters => if some is missing,
-# component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+import PureCloudPlatformClientV2 as pc2
+from PureCloudPlatformClientV2.rest import ApiException
+
+KEY_CLIENT_ID = 'client_id'
+KEY_PASSWORD = '#password'
+KEY_CLOUD_URL = 'cloud_url'
+KEY_DAYS = 'last_days_interval'
+KEY_START = 'start_date'
+
+REQUIRED_PARAMETERS = [KEY_CLIENT_ID, KEY_PASSWORD, KEY_CLOUD_URL]
 REQUIRED_IMAGE_PARS = []
 
 
@@ -34,48 +37,157 @@ class Component(ComponentBase):
         super().__init__()
 
     def run(self):
-        '''
-        Main execution code
-        '''
+        logging.info("Memory usage BRK1> %s" % str(psutil.Process().memory_info().rss))
 
-        # ####### EXAMPLE TO REMOVE
-        # check for missing configuration parameters
         self.validate_configuration_parameters(REQUIRED_PARAMETERS)
         self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         params = self.configuration.parameters
-        # Access parameters in data/config.json
-        if params.get(KEY_PRINT_HELLO):
-            logging.info("Hello World")
 
-        # get last state data/in/state.json from previous run
-        previous_state = self.get_state_file()
-        logging.info(previous_state.get('some_state_parameter'))
+        if params.get(KEY_DAYS):
+            DAYS_COUNT = int(params.get(KEY_DAYS))
+        else:
+            DAYS_COUNT = 3
 
-        # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+        if params.get(KEY_START):
+            from_datetime = datetime.datetime.strptime(params.get(KEY_START), '%Y-%m-%d')
+        else:
+            # get data from previous calendar day only
+            from_datetime = datetime.datetime.combine(
+                datetime.datetime.utcnow(),
+                datetime.time(00, 00, 00)
+            ) - datetime.timedelta(days=DAYS_COUNT)
 
-        # get file path of the table (data/out/tables/Features.csv)
-        out_table_path = table.full_path
-        logging.info(out_table_path)
+        end_date = from_datetime + datetime.timedelta(days=DAYS_COUNT)
+        filter = {
+            "interval": "%s/%s" % (
+                from_datetime.isoformat(timespec="seconds"),
+                end_date.isoformat(timespec="seconds")
+            )
+        }
 
-        # DO whatever and save into out_table_path
-        with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
-            writer = csv.DictWriter(out_file, fieldnames=['timestamp'])
+        # obtain data
+        logging.info("Memory usage BRK2> %s" % str(psutil.Process().memory_info().rss))
+        api_client = pc2.api_client.ApiClient(
+            host=params.get(KEY_CLOUD_URL)
+        ).get_client_credentials_token(params.get(KEY_CLIENT_ID), params.get(KEY_PASSWORD))
+
+        usersApi = pc2.UsersApi(api_client=api_client)
+
+        # @note: login_details[user_id][date] = first mention in day
+        login_details = dict()
+
+        # @note: mapping between emails and names
+        names = dict()
+
+        try:
+            api_response = usersApi.post_analytics_users_details_jobs(filter)
+            async_job_id = api_response.job_id
+
+            counter = 30
+            while counter > 0:
+                job_status = usersApi.get_analytics_users_details_job(async_job_id)
+                if job_status.state == "FULFILLED":
+                    break
+                time.sleep(1)
+
+            counter -= 1
+
+            if counter == 0:
+                # job is not finished in X seconds
+                sys.exit(1)
+
+            # job is ready
+            cursor = None
+
+            while True:
+                logging.info("Memory usage BRK3> %s" % str(psutil.Process().memory_info().rss))
+                if cursor is None:
+                    response = usersApi.get_analytics_users_details_job_results(async_job_id)
+                else:
+                    response = usersApi.get_analytics_users_details_job_results(async_job_id, cursor=cursor)
+
+                cursor = response.cursor
+
+                for user_detail in response.user_details:
+                    detail = usersApi.get_user(user_detail.user_id)
+                    user_name = detail.username
+
+                    names[user_name] = detail.name
+
+                    if user_name not in login_details:
+                        login_details[user_name] = dict()
+
+                    if user_detail.primary_presence is not None:
+                        presence_from = user_detail.primary_presence
+                    else:
+                        presence_from = user_detail.routing_status
+
+                    for presence in presence_from:
+                        if hasattr(presence, "presence.system_presence") and presence.system_presence in ["OFFLINE"]:
+                            continue
+
+                        if hasattr(presence, "presence.routing_status") and presence.routing_status in ["OFF_QUEUE"]:
+                            continue
+
+                        # datetime is in UTC, so we want to move it to local timezone
+                        start_time_utc = presence.start_time
+                        start_time_local = start_time_utc.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+
+                        date_string = str(start_time_local.date())
+
+                        if date_string < str(from_datetime.date()):
+                            # @workaroud: Why genesis returns date outside the date-scope?
+                            # skip these records
+                            continue
+
+                        if date_string not in login_details[user_name]:
+                            login_details[user_name][date_string] = start_time_local
+
+                        login_details[user_name][date_string] = min(
+                            login_details[user_name][date_string],
+                            start_time_local
+                        )
+
+                if cursor is None:
+                    break
+        except ApiException as e:
+            print("Exception when calling ConversationsApi->post_analytics_users_detail_query: %s\n" % e)
+            sys.exit(1)
+
+        # datetime object to proper datetime string
+        for detail in login_details:
+            for record in login_details[detail]:
+                login_details[detail][record] = str(login_details[detail][record])
+
+        # export to CSV (email; name; date; datetime) with PK (email; date)
+        with open('data_file.csv', 'w') as data_file:
+            csv_writer = csv.writer(data_file)
+
+            csv_writer.writerow(["email", "name", "date", "first_login"])
+            for user in login_details:
+                for date in login_details[user]:
+                    csv_writer.writerow([user, names[user], date, login_details[user][date]])
+
+        logging.info("Save table")
+        logging.info("Memory usage BRK-SAVE> %s" % str(psutil.Process().memory_info().rss))
+        conversation_table = self.create_out_table_definition(
+             'worktime.csv', incremental=True, primary_key=['email', 'date'])
+        with open(conversation_table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
+            writer = csv.DictWriter(
+                out_file,
+                fieldnames=['email', 'name', 'date', 'first_login']
+            )
             writer.writeheader()
-            writer.writerow({"timestamp": datetime.now().isoformat()})
 
-        # Save table manifest (output.csv.manifest) from the tabledefinition
-        self.write_manifest(table)
+            for user in login_details:
+                for date in login_details[user]:
+                    writer.writerow(
+                        {"email": user, "name": names[user], "date": date, "first_login": login_details[user][date]}
+                    )
 
-        # Write new state - will be available next run
-        self.write_state_file({"some_state_parameter": "value"})
-
-        # ####### EXAMPLE TO REMOVE END
+        self.write_manifest(conversation_table)
 
 
-"""
-        Main entrypoint
-"""
 if __name__ == "__main__":
     try:
         comp = Component()
@@ -83,7 +195,7 @@ if __name__ == "__main__":
         comp.execute_action()
     except UserException as exc:
         logging.exception(exc)
-        exit(1)
+        sys.exit(1)
     except Exception as exc:
         logging.exception(exc)
-        exit(2)
+        sys.exit(2)
